43ab7e098ee48ece70fe2636daf17883deee7b31
Adan Abbi <adanweli@hotmail.com>
2021-12-06 02:08:45 -0800

    added some css styles

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const createArticle = (event) => {
   event.preventDefault();
-  const articleTitleElement = document.querySelector("#article-title");
-  const articleContentElement = document.querySelector("#article-content");
-  const articleSection = document.querySelector("#article-section");
+  const articleTitleElement = document.querySelector("#article-title"); // title input
+  const articleContentElement = document.querySelector("#article-content"); // text area input
+  const articleSection = document.querySelector("#article-section"); // first article element
 
-  const nextArticle = document.createElement("ARTICLE");
-  const articleHeading = document.createElement("H3");
-  const articleParagraph = document.createElement("P");
+  const nextArticle = document.createElement("ARTICLE"); // article element
+  const articleHeading = document.createElement("H3"); // heading 3
+  const articleParagraph = document.createElement("P"); //paragragh
 
   nextArticle.append(articleHeading);
   nextArticle.append(articleParagraph);
